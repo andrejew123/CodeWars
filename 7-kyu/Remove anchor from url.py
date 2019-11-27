df9594a1841fcd97f9b8d88e8efd7cb6c3e3f129
andrejew123 <i.andrejewicz@gmail.com>
2019-11-27 19:34:33 +0100

    fixup! Added new 7 kata for removing anchor from url

--- 7-kyu/Remove anchor from url.py
+++ 7-kyu/Remove anchor from url.py
@@ -12,6 +12,14 @@ def remove_url_anchor(url):
     return url.partition("#")[0]
 
 
+def remove_url_anchor_2(url):
+    return url.split("#")[0]
+
+
 assert remove_url_anchor("www.codewars.com#about") == "www.codewars.com"
 assert remove_url_anchor("www.codewars.com/katas/?page=1#about") == "www.codewars.com/katas/?page=1"
 assert remove_url_anchor("www.codewars.com/katas/") == "www.codewars.com/katas/"
+
+assert remove_url_anchor_2("www.codewars.com#about") == "www.codewars.com"
+assert remove_url_anchor_2("www.codewars.com/katas/?page=1#about") == "www.codewars.com/katas/?page=1"
+assert remove_url_anchor_2("www.codewars.com/katas/") == "www.codewars.com/katas/"
